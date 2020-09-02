6943154f0249a7d1025d7df7e6825618e625429e
ImmArkorful <e.arkorful3@gmail.com>
2020-09-02 23:24:39 +0000

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,50 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'none',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node.modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-  },
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+  return {
+    mode: 'none',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node.modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              // options: {
+              //   sourceMaps: isProduction ? 'source-map' : 'inline-source-map',
+              // },
+            },
+            {
+              loader: 'sass-loader',
+              // options: {
+              //   sourceMaps: isProduction ? 'source-map' : 'inline-source-map',
+              // },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
 };
