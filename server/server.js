89fa92a5087c904d59b90f0253dcb10ad65c6d61
ImmArkorful <e.arkorful3@gmail.com>
2020-09-03 18:59:20 +0000

    fix: port fixed

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
-const port = 3000 || process.env.PORT;
+const port =  process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
